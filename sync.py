69833817f5e219ced425353104ebdb45c01c724c
John Clear <johnclear4@gmail.com>
2023-09-11 12:19:49 -0700

    Add exception handling

--- sync.py
+++ sync.py
@@ -1,4 +1,8 @@
 import datetime
+import traceback
+
+import canvasapi.exceptions
+import trello.exceptions
 
 from canvashelper import get_upcoming_assignments
 from trellohelper import add_assignments
@@ -10,8 +14,12 @@ def main():
     print(f'[{datetime.datetime.now()}] Checking assignments...')
 
     for class_id in CLASS_LIST_DICT.keys():
-        upcoming_assignments = get_upcoming_assignments(class_id)
-        add_assignments(class_id, upcoming_assignments)
+        try:
+            upcoming_assignments = get_upcoming_assignments(class_id)
+            add_assignments(class_id, upcoming_assignments)
+        except trello.exceptions.ResourceUnavailable or canvasapi.exceptions.CanvasException as e:
+            print(f'Error fetching assignments and updating cards for course: {class_id}')
+            traceback.print_exc()
 
 
 if __name__ == '__main__':
